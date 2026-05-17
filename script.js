65ddce1cd0a7382c37a4fd60372a462b6da912cc
Anmol Kumar Singh <eranmol2007@gmail.com>
2026-05-17 16:23:31 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const chatForm = document.getElementById('chatForm');
 const userInput = document.getElementById('userInput');
 const chatBox = document.getElementById('chatBox');
 
-const API_KEY = "AIzaSyAFCQlES7mdmKKoFjScIQ9b_8-QpJGjSnw"; 
+const API_KEY = "YOUR_API_KEY"; 
 
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
